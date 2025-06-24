a9a77bcc85ad42ab0f472a23d09c8ecc4c32e72e
graslowsnail <graslowsnail@gmail.com>
2025-06-24 12:03:33 -0400

    create seperate page for insertion sort

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,15 @@
-'use client'
-import { useState } from 'react';
-import { createArray } from './utils/insertionSort';
-import { insertionSort } from './utils/insertionSort';
+import Link from 'next/link'
 
 export default function Home() {
-  const [userInput, setUserInput] = useState('')
-  const [userArray, setArray] = useState<number []>([])
-
-  const handleClick = () => {
-    const num = Number(userInput)
-    if(num > 1000) {
-        alert("Max 1000!")
-        return
-    }
-    const newArray = createArray(num)
-    setArray(newArray)
-    console.log('temp array in componet ', newArray)
-  }
-  const handleSortClick = () => {
-    //const sortedArray = sortArray(userArray)
-    const sortedArray = insertionSort(userArray)
-    console.log(sortedArray)
-  }
-
   return (
-    <div className="min-h-screen p-8">
-      <main>
-        <h1>Sorting Visualizer</h1>
-        <p>Your sorting algorithm visualization will go here!</p>
-        <input
-          type='number'
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Enter a number from 1-1000 "
-          className='border border-black'
-        />
-
-        <button
-          className='border border-black'
-          onClick={handleClick}>
-            submit value
-        </button>
-
-        <button
-          className='border border-black'
-          onClick={handleSortClick}>
-            Sort your array 
-        </button>
-
+    <div className="min-h-screen flex items-center justify-center">
+      <main className="text-center">
+        <h1 className="mb-4">Yoo select a algo to Visualize!</h1>
+        <Link href="/insertionSort">
+          <button className="border border-black px-4 py-2">
+            Insertion Sort
+          </button>
+        </Link>
       </main>
     </div>
   );
