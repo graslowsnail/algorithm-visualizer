120ea7068851f9deaad19e1afaa67a0cba1a846a
graslowsnail <graslowsnail@gmail.com>
2025-06-27 09:34:01 -0400

    added selection sort builds

---
+++ app/utils/selectionSort/selectionSort3.ts
@@ -0,0 +1,17 @@
+
+
+const selectionSort3 = () => {
+    const arr = [91, 37, 68, 14, 82, 45, 23, 77, 56, 39]
+
+    for(let i = 0; i< arr.length; i ++ ) {
+        let minIndex = i
+        for(let j = i; j < arr.length; j ++){
+            if( arr[j] > arr[minIndex] ) {
+                minIndex = j
+            }
+        }
+        let cachedIndex = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = cachedIndex
+    }
+}
